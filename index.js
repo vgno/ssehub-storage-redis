103c398616d8bd901e4ceda25cac859b7a068cd9
Espen Hovlandsdal <rexxars@gmail.com>
2015-05-29 15:39:33 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var redis = require('redis');
+var util = require('util');
+var events = require('events');
+
+function RedisStorage(config) {
+    this.config = config;
+}
+
+util.inherits(RedisStorage, events.EventEmitter);
+
+RedisStorage.prototype.connect = function(callback) {
+    if (!this.client) {
+        this.client = redis.createClient(
+            this.config.redisPort,
+            this.config.redisHost,
+            this.config.redisOptions
+        );
+
+        this.client.on('error', function(e) {
+            this.emit('error', e);
+        }.bind(this));
+
+        if (this.config.redisDbNum !== 0) {
+            this.client.select(this.config.redisDbNum, callback);
+            return this.client;
+        }
+    }
+
+    if (callback) {
+        process.nextTick(callback);
+    }
+
+    return this.client;
+};
+
+RedisStorage.prototype.storeMessage = function(path, msg, callback) {
+    if (!this.client) {
+        this.connect();
+    }
+
+    var maxItems = this.config.maxHistoryItems || 500;
+
+    // Push the message onto the list
+    this.client.lpush(path, JSON.stringify(msg), function(err, length) {
+        if (err) {
+            return callback(err);
+        }
+
+        // If we have more items than allowed, pop the oldest one of the list
+        if (length > maxItems) {
+            this.client.rpop(path, callback);
+        } else {
+            callback();
+        }
+    }.bind(this));
+};
+
+RedisStorage.prototype.getMessages = function(path, since, callback) {
+    if (!this.client) {
+        this.connect();
+    }
+
+    if (typeof since === 'function' && !callback) {
+        callback = since;
+        since = null;
+    }
+
+    this.client.lrange(path, 0, -1, function(err, items) {
+        if (err) {
+            return callback(err);
+        }
+
+        items = items.map(JSON.parse);
+
+        callback(null, since ? items.filter(function(item) {
+            return item.id > since;
+        }) : items);
+    });
+};
+
+module.exports = RedisStorage;
---
+++ test/redis-storage.test.js
@@ -0,0 +1,146 @@
+'use strict';
+
+var runAfter = require('lodash.after');
+var merge = require('lodash.merge');
+var expect = require('chai').expect;
+var RedisStorage = require('../');
+var redis = require('redis');
+
+var config = {
+    redisPort: process.env.REDIS_PORT || 6379,
+    redisHost: process.env.REDIS_HOST || 'localhost',
+    redisDbNum: 15
+};
+
+describe('redis storage', function() {
+    var storage;
+
+    beforeEach(function() {
+        storage = new RedisStorage(config);
+    });
+
+    afterEach(function(done) {
+        var client = redis.createClient(
+            config.redisPort,
+            config.redisHost
+        );
+
+        client.select(config.redisDbNum, function() {
+            client.flushdb();
+            done();
+        });
+    });
+
+    it('implicitly connects', function(done) {
+        storage.getMessages('/some/path', function(err) {
+            failOnError(err);
+            done();
+        });
+    });
+
+    it('can explicitly be told to connect', function(done) {
+        storage.connect();
+        storage.getMessages('/some/path', function(err) {
+            failOnError(err);
+            done();
+        });
+    });
+
+    it('calls callback on connect', function(done) {
+        storage.connect(done);
+    });
+
+    it('only connects once', function(done) {
+        var client1 = storage.connect();
+        var client2 = storage.connect();
+        var client3 = storage.connect(done);
+
+        expect(client1).to.equal(client2);
+        expect(client1).to.equal(client3);
+    });
+
+    it('doesnt select a database if db 0 is specified', function(done) {
+        var store = new RedisStorage(merge({}, config, { redisDbNum: 0 }));
+        store.connect(done);
+    });
+
+    it('can set and get messages', function(done) {
+        var path = '/some/path';
+        storage.storeMessage(path, { data: 'foobar' }, function(err) {
+            failOnError(err);
+
+            storage.getMessages(path, null, function(getErr, msgs) {
+                failOnError(getErr);
+                expect(msgs).to.have.length(1);
+                expect(msgs[0].data).to.equal('foobar');
+                done();
+            });
+        });
+    });
+
+    it('doesnt blow up if storeMessage is called after explicitly connecting', function(done) {
+        var path = '/some/path';
+        storage.connect();
+        storage.storeMessage(path, { data: 'foobar' }, function(err) {
+            failOnError(err);
+            done();
+        });
+    });
+
+    it('doesnt blow up if getMessages is called after explicitly connecting', function(done) {
+        var path = '/some/path';
+        storage.connect();
+        storage.getMessages(path, function(err) {
+            failOnError(err);
+            done();
+        });
+    });
+
+    it('can filter messages since a given ID', function(done) {
+        var path = '/some/path', id = 0;
+
+        while (++id < 50) {
+            storage.storeMessage(path, { id: id, data: 'Message #' + id }, failOnError);
+        }
+
+        storage.getMessages(path, 40, function(err, messages) {
+            failOnError(err);
+            expect(messages).to.have.length(9);
+            expect(messages[0].data).to.equal('Message #49');
+            expect(messages[8].data).to.equal('Message #41');
+            done();
+        });
+    });
+
+    it('lifts client errors events to storage error events', function(done) {
+        var err = new Error('Some error');
+
+        storage.on('error', function(liftedError) {
+            expect(liftedError).to.equal(err);
+            done();
+        });
+
+        storage.connect().emit('error', err);
+    });
+
+    it('caps at the maximum given history length', function(done) {
+        var store = new RedisStorage(merge({}, config, { maxHistoryItems: 5 }));
+        var path = '/some/path', id = 0;
+
+        var checkMsgs = runAfter(50, function() {
+            store.getMessages(path, function(err, messages) {
+                failOnError(err);
+                expect(messages).length.to.be(5);
+                done();
+            });
+        });
+
+        while (++id <= 50) {
+            store.storeMessage(path, { id: id, data: 'Message #' + id }, checkMsgs);
+        }
+    });
+});
+
+function failOnError(err) {
+    expect(err).to.not.be.ok;
+}
